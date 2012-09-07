8e879164c679e746234dc93c3dde868eb73b237f
RealityFork <mystx@chaostheory.co.nz>
2012-09-07 14:55:43 +1200

    Fixed Disclaimers.

--- debugrender.h
+++ debugrender.h
@@ -1,7 +1,4 @@
 /*
-    Copyright (C) 2011 Allen Jordan ()
-    Copyright (C) 2011 Xabier Larrakoetxea (slok)
-
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
